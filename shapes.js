59540c7f2c07cdb93f11315c62741d77fa872b58
flandish <frankconway1@gmail.com>
2020-09-20 17:01:08 +0100

    Cool nested, thin lined noise blobs!

--- shapes.js
+++ shapes.js
@@ -6,31 +6,41 @@
 
 var round = new Path();
 var a = 0;
-var r = 600;
-var segs = 400;
+var r = 400;
+var segs = 100;
 
-noise.seed(Math.random())
-for(var j=0; j<30; j++){
-    for(var i=0; i <=segs; i++){
-        var x = r * Math.cos(a);
-        var y = r * Math.sin(a);
-        round.add(new Segment(x,y))
-        //console.log(x)
-        a += Math.PI*2/segs
-         r += noise.perlin2(x,y) * segs/20;
-        //r += Math.random() * 4;
-    }
-} // this make one big spiral, not multiple "round" objects.
+for(var i=0; i <=segs; i++){
+    noise.seed(Math.random())
+    var x = r * Math.cos(a);
+    var y = r * Math.sin(a);
+    round.add(new Segment(x,y))
+    a += Math.PI*2/segs;
+    r += noise.perlin2(x,y) * segs/5;
+}
+
+for(var i=0; i<=100; i++){
+    var roundClone = round.clone();
+    roundClone.scale(0.3 + (i/100));
+    //roundClone.selected = 'true'
+    roundClone.strokeColor = 'black';
+    roundClone.position = new Point(view.center);
+    roundClone.smooth();
+}
+
+
+
+
+// this make one big spiral, not multiple "round" objects.
     // this explains the lines that intersect it. (closing the shape)
 
-console.log(round);
-round.strokeColor = '#5f9ea0';
-round.fillColor = 'black';
-round.strokeWidth = 3;
-round.closed=true;
-round.selected=false;
+//console.log(round);
+//round.strokeColor = '#5f9ea0';
+//round.fillColor = 'black';
+//round.strokeWidth = 3;
+round.closed = true;
+//round.selected = true;
 round.smooth();
-round.position = new Point(view.center)
+round.position = new Point(view.center);
 
 //for number of points
 // change them with perlin
